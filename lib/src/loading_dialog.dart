1fc824b3ae0202cc0175f58817179fa51afb8a26
Volodymyr Sydorko <v.sydorko@softonix.org>
2024-05-22 21:27:49 +0300

    Update deps

--- lib/src/loading_dialog.dart
+++ lib/src/loading_dialog.dart
@@ -4,13 +4,15 @@ class LoadingDialog extends StatelessWidget {
   static bool loaderShown = false;
 
   static void show(BuildContext context, {Key? key}) {
-    loaderShown = true;
-    showDialog<void>(
-      context: context,
-      useRootNavigator: true,
-      barrierDismissible: true,
-      builder: (_) => LoadingDialog(key: key),
-    ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
+    if (!loaderShown) {
+      loaderShown = true;
+      showDialog<void>(
+        context: context,
+        useRootNavigator: true,
+        barrierDismissible: true,
+        builder: (_) => LoadingDialog(key: key),
+      ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
+    }
   }
 
   static void hide(BuildContext context) {
